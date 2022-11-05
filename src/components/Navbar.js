27ea50d5a3386b5eb48a62e8061aa966be8d6598
Pardeep Singh <PARDEEPSINGHBATTH@gmail.com>
2022-11-05 18:19:54 +0530

    second

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar fixed-top navbar-expand-lg bg-light ">
         <div className="container">
           
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
